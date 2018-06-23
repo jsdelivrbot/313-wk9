7efab6808cc9782f90e672ad58e6c6563a9a10af
danverde <gre12014@byui.edu>
2018-06-23 13:39:30 -0600

    wk9 WIP

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 5000;
 
 express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+    .use(express.static(path.join(__dirname, 'public')))
+    .set('views', path.join(__dirname, 'views'))
+    .set('view engine', 'ejs')
+    .get('/', (req, res) => res.render('pages/index'))
+    .get('/form', (req, res) => res.render('pages/form'))
+    .get('/result', (req, res) => res.redirect('form'))
+    .post('/result', (req, res) => {
+        res.render('pages/result');
+    })
+    .listen(PORT, () => console.log(`Listening on ${ PORT }`));
